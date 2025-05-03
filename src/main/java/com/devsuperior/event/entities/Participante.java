413d0744582fa520a6a80c3d65a3db1ed889a6b0
Lucas Costa <lucas.desenvolvedor.tec@gmail.com>
2025-05-02 23:47:30 -0300

    Participante

--- src/main/java/com/devsuperior/event/entities/Participante.java
+++ src/main/java/com/devsuperior/event/entities/Participante.java
@@ -2,56 +2,22 @@ package com.devsuperior.event.entities;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "tb_participante")
 public class Participante {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String name;
-    private String descricao;
-    private Double preco;
+    private Integer id;
+    private String nome;
+    private String email;
 
-    public Participante() {
-    }
+    @ManyToMany
 
-    public Participante(Long id, String name, String descricao, Double preco) {
-        this.id = id;
-        this.name = name;
-        this.descricao = descricao;
-        this.preco = preco;
-    }
+    private List<Atividade> atividades = new ArrayList<>();
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public Double getPreco() {
-        return preco;
-    }
-
-    public void setPreco(Double preco) {
-        this.preco = preco;
-    }
 }
